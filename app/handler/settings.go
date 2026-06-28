ea8bd1e4327aae4345c1b11089b9b89a12384222
Thomas Kastner <tom@sprungknoedl.at>
2026-06-28 22:41:10 +0200

    Add settings overview page replacing sidebar cog dropdown

--- app/handler/settings.go
+++ app/handler/settings.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/sprungknoedl/dagobert/app/auth"
 	"github.com/sprungknoedl/dagobert/app/model"
+	"github.com/sprungknoedl/dagobert/app/views"
 )
 
 type SettingsCtrl struct {
@@ -11,4 +14,8 @@ type SettingsCtrl struct {
 
 func NewSettingsCtrl(store *model.Store, acl *auth.ACL) *SettingsCtrl {
 	return &SettingsCtrl{BaseCtrl{store, acl}}
+}
+
+func (ctrl SettingsCtrl) Overview(w http.ResponseWriter, r *http.Request) {
+	Render(w, r, http.StatusOK, views.SettingsOverview(Env(ctrl, r)))
 }
